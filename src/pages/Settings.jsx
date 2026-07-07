469cee17cf4a7aadfd3c50916f59c83aaed4ea83
BTC001B <btc001b.btcglobal@gmail.com>
2026-07-07 15:58:55 +0530

    compose-styling

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -34,7 +34,7 @@ const quillModules = {
     [{ 'header': [1, 2, false] }],
     ['bold', 'italic', 'underline', 'strike', 'blockquote'],
     [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-    ['link', 'image'],
+    ['link'],
     ['clean']
   ],
   imageResize: {
